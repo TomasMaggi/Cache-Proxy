5350aa1d6efc098d8c8d16d834d93dd59e644a4c
TomasMaggi <87153661+TomasMaggi@users.noreply.github.com>
2025-02-16 23:35:07 -0300

    Concurrency handling (#2)
    * add multi request handling
    
    * Fix scope access in ps for testing

--- Cache-Proxy/Program.cs
+++ Cache-Proxy/Program.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections;
-using System.IO;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
+using System.Threading;
 
 class CacheProxyServer
 {
-    // In memory cache
-    static Dictionary<string, string> Cache = new Dictionary<string, string>();
-
-    // TODO: add a reference to sqlite for non frecuen used deterministic request
-
     public static void Main(string[] args)
     {
         TcpListener server = null;
@@ -21,15 +14,10 @@ class CacheProxyServer
             Int32 port = 13000;
             IPAddress localAddr = IPAddress.Parse("127.0.0.1");
 
-            // TcpListener server = new TcpListener(port);
             server = new TcpListener(localAddr, port);
 
             // Start listening for client requests.
             server.Start();
-
-            // Buffer for reading data
-            Byte[] bytes = new Byte[256];
-            String data = null;
 
             // Enter the listening loop.
             while (true)
@@ -37,45 +25,60 @@ class CacheProxyServer
                 Console.Write("Waiting for a connection... ");
 
                 // Perform a blocking call to accept requests.
-                // You could also use server.AcceptSocket() here.
-                using TcpClient client = server.AcceptTcpClient();
-                Console.WriteLine("Connected!");
+                TcpClient client = server.AcceptTcpClient();
 
-                data = null;
-
-                // Get a stream object for reading and writing
-                NetworkStream stream = client.GetStream();
-
-                int i;
-
-                // Loop to receive all the data sent by the client.
-                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-                {
-                    // Translate data bytes to a ASCII string.
-                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                    Console.WriteLine("Received: {0}", data);
-
-                    // Process the data sent by the client.
-                    data = data.ToUpper();
-
-                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-
-                    // Send back a response.
-                    stream.Write(msg, 0, msg.Length);
-                    Console.WriteLine("Sent: {0}", data);
-                }
+                // Spawn a new thread to handle the client request
+                Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClient));
+                clientThread.Start(client);
             }
         }
         catch (SocketException e)
         {
             Console.WriteLine("SocketException: {0}", e);
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Unkown Exception: {0}", ex);
+        }
         finally
         {
-            server.Stop();
+            server?.Stop();
         }
 
         Console.WriteLine("\nHit enter to continue...");
         Console.Read();
+    }
+
+    private static void HandleClient(object obj)
+    {
+        // Cast the object to TcpClient
+        using TcpClient client = (TcpClient)obj;
+        Console.WriteLine("Connected!");
+
+        // Buffer for reading data
+        Byte[] bytes = new Byte[256];
+        String? data = null;
+
+        // Get a stream object for reading and writing
+        NetworkStream stream = client.GetStream();
+
+        int i;
+
+        // Loop to receive all the data sent by the client.
+        while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+        {
+            // Translate data bytes to a ASCII string.
+            data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+            Console.WriteLine("Received: {0}", data);
+
+            // Process the data sent by the client.
+            data = data.ToUpper();
+
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+            // Send back a response.
+            stream.Write(msg, 0, msg.Length);
+            Console.WriteLine("Sent: {0}", data);
+        }
     }
 }
